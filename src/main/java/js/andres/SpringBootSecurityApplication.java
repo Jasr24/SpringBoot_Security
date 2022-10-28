85226ceb39cfda0ac7397e1cc9af139415f4b09d
Andres <josex.1995@hotmail.com>
2022-10-28 10:29:59 -0500

    primer commit, creacion del projecto con las despencencia thymeleaf, weeb, y web tool, NOTA: la seguridad la agregaremos manualente

---
+++ src/main/java/js/andres/SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package js.andres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurityApplication.class, args);
+	}
+
+}
